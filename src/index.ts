80bfd2a107d81fa4758db91e29fb4484636b674e
Stan Choi <sjc591@nyu.edu>
2025-06-25 20:47:47 -0400

    add custom handler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import express from "express";
 
 const app = express();
+const PORT = 8080;
 
-app.listen(8080);
+function handlerReadiness(req: express.Request, res: express.Response) {
+  res.set("Content-Type", "text/plain; charset=utf8");
+  res.send("OK");
+}
+
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", handlerReadiness);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
