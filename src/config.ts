9506fd929f3a568832f5bb473da59a9f8301742a
Stan Choi <sjc591@nyu.edu>
2025-07-05 14:01:57 -0400

    Add drizzle queries

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
 type ApiConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
+
+process.loadEnvFile();
+
+function envOrThrow(key: string) {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not set`);
+  }
+  return value;
+}
 
 export const config: ApiConfig = {
   fileserverHits: 0,
+  dbURL: envOrThrow("DB_URL"),
 };
